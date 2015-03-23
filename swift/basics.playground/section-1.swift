2806e5a5203160dfb56d389012b69ac38f57a21e
Hugo Lu <printk@gmail.com>
2015-03-23 09:53:03 +0800

    basics of swift

---
+++ swift/basics.playground/section-1.swift
@@ -0,0 +1,11 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+// var and constant
+let constant = 10
+var variable = 0
+
+// type annotation
+var greeting:String = "hello world"
+
